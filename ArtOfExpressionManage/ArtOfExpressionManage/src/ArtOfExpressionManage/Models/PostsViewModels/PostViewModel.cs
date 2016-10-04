f0927329d2dbf3cc1650013df05ac5a1ca1b8056
Avishai Faitelson <avishai.faitelson@gmail.com>
2016-10-04 23:56:19 +0300

    Added New Project

---
+++ ArtOfExpressionManage/ArtOfExpressionManage/src/ArtOfExpressionManage/Models/PostsViewModels/PostViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArtOfExpressionManage.Models.PostsViewModels
+{
+    public class PostViewModel
+    {
+        public int PostId { get; set; }
+        [Required]
+        public string PostTitle { get; set; }
+        [Required]
+        public string PostContent { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+
+        public static void Stam()
+        {
+
+        }
+    }
+}
